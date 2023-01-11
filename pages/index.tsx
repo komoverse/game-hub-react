91b7c3e0b7d1d7b5cb795b9c0463e5c8ac706256
Erick Hilda <erick.hilda@ajaib.co.id>
2023-01-11 21:11:54 +0700

    chore: add carrousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,142 @@
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Link from "components/Link";
-import ProTip from "components/ProTip";
-import Copyright from "components/Copyright";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+import Carrousel from "@/components/Carrousel";
+import { Box, Container, Paper } from "@mui/material";
 
-export default function Home() {
+const mainFeaturedPost = {
+  title: "Title of a longer featured blog post",
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: "https://source.unsplash.com/random",
+  imageText: "main image description",
+  linkText: "Continue reading…",
+};
+export default function MainFeaturedPost() {
+  const [sliderRef, instanceRef] = useKeenSlider(
+    {
+      slideChanged() {
+        console.log("slide changed");
+      },
+    },
+    [
+      // add plugins here
+    ]
+  );
+
   return (
-    <Container maxWidth="lg">
+    // <Paper
+    //   sx={{
+    //     position: "relative",
+    //     backgroundColor: "grey.800",
+    //     color: "#fff",
+    //     mb: 4,
+    //     backgroundSize: "cover",
+    //     backgroundRepeat: "no-repeat",
+    //     backgroundPosition: "center",
+    //     backgroundImage: `url(${mainFeaturedPost.image})`,
+    //     height: "100%",
+    //   }}
+    // >
+    //   {/* Increase the priority of the hero background image */}
+    //   {
+    //     <img
+    //       style={{ display: "none" }}
+    //       src={mainFeaturedPost.image}
+    //       alt={mainFeaturedPost.imageText}
+    //     />
+    //   }
+    //   <Box
+    //     sx={{
+    //       position: "absolute",
+    //       top: 0,
+    //       bottom: 0,
+    //       right: 0,
+    //       left: 0,
+    //       backgroundColor: "rgba(0,0,0,.3)",
+    //     }}
+    //   />
+    //   <Grid container>
+    //     <Grid item md={6}>
+    //       <Box
+    //         sx={{
+    //           position: "relative",
+    //           p: { xs: 3, md: 6 },
+    //           pr: { md: 0 },
+    //         }}
+    //       >
+    //         <Typography
+    //           component="h1"
+    //           variant="h3"
+    //           color="inherit"
+    //           gutterBottom
+    //         >
+    //           {mainFeaturedPost.title}
+    //         </Typography>
+    //         <Typography variant="h5" color="inherit" paragraph>
+    //           {mainFeaturedPost.description}
+    //         </Typography>
+    //         <Link variant="subtitle1" href="#">
+    //           {mainFeaturedPost.linkText}
+    //         </Link>
+    //       </Box>
+    //     </Grid>
+    //   </Grid>
+    // </Paper>
+    <Container
+      disableGutters
+      maxWidth={false}
+      sx={{
+        height: "100%",
+      }}
+    >
       <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
+        component="div"
+        ref={sliderRef}
+        className="keen-slider"
+        sx={{ height: { xs: "50%", md: "100%" } }}
       >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
+        <Paper
+          className="keen-slider__slide"
+          sx={{
+            position: "relative",
+            backgroundColor: "grey.800",
+            color: "#fff",
+            mb: 4,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            height: { xs: "50%", md: "100%" },
+          }}
+        ></Paper>
+        <Paper
+          className="keen-slider__slide"
+          sx={{
+            position: "relative",
+            backgroundColor: "grey.800",
+            color: "#fff",
+            mb: 4,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            height: { xs: "50%", md: "100%" },
+          }}
+        ></Paper>
+        <Paper
+          className="keen-slider__slide"
+          sx={{
+            position: "relative",
+            backgroundColor: "grey.800",
+            color: "#fff",
+            mb: 4,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            height: { xs: "50%", md: "100%" },
+          }}
+        ></Paper>
       </Box>
     </Container>
   );
