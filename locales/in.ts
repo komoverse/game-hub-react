026dd13761f3dee67054cab1da387b83321bc48a
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-01-23 23:47:45 +0700

    add translator for home page

--- locales/in.ts
+++ locales/in.ts
@@ -1,7 +1,13 @@
 const lang = {
   navbar: {
-    login: "Vāliṭa nāla juṛō",
-    search: "Khōja",
+    login: 'Vāliṭa nāla juṛō',
+    search: 'Khōja',
+  },
+  home: {
+    featured: 'श्रेष्ठता',
+    viewAll: 'सभी देखें',
+    event: 'घटना',
+    newListing: 'नई सूची',
   },
 };
 export default lang;
