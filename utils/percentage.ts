d1fa21fd3675030d5c5163f99b78999390361af2
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-01-25 23:19:17 +0700

    add global function format percent

---
+++ utils/percentage.ts
@@ -0,0 +1,6 @@
+export const formatPercent = (percent: number) =>
+  Intl.NumberFormat('en-US', {
+    style: 'percent',
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+  }).format(percent / 100);
