7a8880d0fc861dac6c45a1532971fd6a87717965
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-01-25 23:20:15 +0700

    set global variable tag and color 🎨

--- utils/globalVariable.ts
+++ utils/globalVariable.ts
@@ -2,4 +2,9 @@ export const KomoverseTag = 'Komoverse';
 export const COLOR = {
   baseGreen: '#0FD236',
   baseWhite: '#FFFFFF',
+  baseSemiBlack: '#141414',
+  baseGray: '#B5B5B5',
+  backgroundCardSemiBlack: '#111111',
+  backgroundRoot: '#000000DD',
+  borderSemiBlack: '#1E1E1E',
 };
