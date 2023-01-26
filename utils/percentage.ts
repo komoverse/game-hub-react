31b12fdcf26eac1f0b55fef8703aac85c71b8a4a
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-01-26 14:35:16 +0700

    add new param percent

--- utils/percentage.ts
+++ utils/percentage.ts
@@ -1,5 +1,5 @@
-export const formatPercent = (percent: number) =>
-  Intl.NumberFormat('en-US', {
+export const formatPercent = (percent: number, format: string) =>
+  Intl.NumberFormat(format, {
     style: 'percent',
     minimumFractionDigits: 1,
     maximumFractionDigits: 1,
