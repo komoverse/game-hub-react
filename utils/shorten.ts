ea030dd3456d17bf33ac5a1a34e604fbc9bb677b
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-01-28 17:24:15 +0700

    bugfix undifined address

--- utils/shorten.ts
+++ utils/shorten.ts
@@ -1,9 +1,9 @@
 export const shortenWalletAddress = (address: string) =>
-  `${address.slice(0, 5)}...${address.slice(address.length - 3)}`;
+  `${address?.slice(0, 5)}...${address?.slice(address?.length - 3)}`;
 
 export const shortenTitleGame = (title: string) => {
-  if (title.length > 12) {
-    return `${title.slice(0, 12)}...`;
+  if (title?.length > 12) {
+    return `${title?.slice(0, 12)}...`;
   }
   return title;
 };
