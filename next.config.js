067b9eabf66b9a0e8f79cd12cdf0d94e0c0be59d
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-01-28 17:22:45 +0700

    add domain images

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   reactStrictMode: true,
   i18n: {
-    locales: ["en", "id", "cn", "in"],
-    defaultLocale: "en",
+    locales: ['en', 'id', 'cn', 'in'],
+    defaultLocale: 'en',
     localeDetection: false,
+  },
+  images: {
+    domains: ['cdn.shyft.to', 'www.arweave.net'],
   },
 };
