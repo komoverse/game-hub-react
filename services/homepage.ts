44c35c22b012b0b1c9d82994e85207f1e22f7b02
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-01-28 17:25:11 +0700

    get list recent and itemId

--- services/homepage.ts
+++ services/homepage.ts
@@ -1,7 +1,19 @@
 import komoverseAxiosIns from 'helper/headers';
-import { FEATURED } from 'helper/url';
+import { FEATURED, MARKET_ITEM_ID, MARKET_RECENT } from 'helper/url';
 
 export const getListFeatured = async () => {
   const { data } = await komoverseAxiosIns.get(FEATURED);
-  return await data.data;
+  return data.data;
+};
+
+export const getListRecent = async () => {
+  const { data } = await komoverseAxiosIns.get(MARKET_RECENT);
+  return data.data;
+};
+
+export const getMarketItemById = async (id: string) => {
+  const { data } = await komoverseAxiosIns.get(
+    MARKET_ITEM_ID.replace('{{id}}', id)
+  );
+  return data.data;
 };
