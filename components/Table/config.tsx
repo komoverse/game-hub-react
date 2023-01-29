b667cee815fa8cca866afc97afd365f2facc77e7
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-01-29 23:46:05 +0700

    create reusable component table and detail nft 🌈

---
+++ components/Table/config.tsx
@@ -0,0 +1,68 @@
+import { dateFromNow } from "@/helper/date";
+import { COLOR } from "@/utils/globalVariable";
+import { Button, Typography } from "@mui/material";
+import { GridColDef } from "@mui/x-data-grid"
+import ViewIcon from '@/components/ViewIcon'
+
+export const columns: GridColDef[] = [
+  {
+    field: 'id',
+    headerName: 'TYPE',
+    minWidth: 166,
+  },
+  {
+    field: 'symbol',
+    headerName: 'SELLER',
+    sortable: false,
+    minWidth: 260,
+  },
+  {
+    field: 'name',
+    headerName: 'BUYER',
+    sortable: false,
+    minWidth: 260,
+  },
+  {
+    field: 'market_cap_rank',
+    headerName: 'PRICE',
+    sortable: false,
+    minWidth: 166,
+  },
+  {
+    field: 'atl_date',
+    headerName: 'TIME',
+    sortable: false,
+    minWidth: 166,
+    renderCell: (params) => (
+      <Typography sx={{ color: COLOR.baseSky }}>
+        {dateFromNow(params.value)}
+      </Typography>
+    ),
+  },
+  {
+    field: 'details',
+    headerName: 'DETAILS',
+    headerAlign: 'center',
+    align: 'center',
+    sortable: false,
+    minWidth: 166,
+    renderCell: (params) => (
+      <Button
+        sx={{
+          border: `1px solid ${COLOR.baseGreen}`,
+          textTransform: 'uppercase',
+          padding: '5px 15px',
+          borderRadius: 1,
+        }}
+        endIcon={<ViewIcon />}
+      >
+        <Typography
+          variant="body1"
+          sx={{ fontSize: '0.75rem', color: COLOR.baseGreen }}
+        >
+          View
+        </Typography>
+      </Button>
+    ),
+  },
+];
