01b6b6cc8a7baf85fb823fd83f662783f2a07e1b
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-01-31 16:55:42 +0700

    send data to redux sidebar

--- services/sidebar/index.ts
+++ services/sidebar/index.ts
@@ -1,9 +1,10 @@
-import komoverseAxiosIns from "helper/headers";
-import { SIDEBAR } from "helper/url";
-import { dataTransformer } from "./helper";
+import komoverseAxiosIns from 'helper/headers';
+import { SIDEBAR } from 'helper/url';
+import { dataTransformer } from './helper';
+import action from '@/store/sidebar/action';
 
 export const getSidebarMenu = async () => {
   const { data } = await komoverseAxiosIns.get(SIDEBAR);
-
+  action.setSideabar(data.data);
   return dataTransformer(data.data);
 };
