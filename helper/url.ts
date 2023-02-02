33f40635bbb392f2ad106e6c33e4225fcc9a8336
Erick Hilda <erickh.andreas@gmail.com>
2023-02-02 19:21:24 +0700

    feat: add game market service

--- helper/url.ts
+++ helper/url.ts
@@ -1,7 +1,9 @@
-export const FEATURED = '/v2/featured';
-export const SIDEBAR = '/v2/sidebar';
-export const MARKET_RECENT = '/v2/market/recent';
-export const MARKET_ITEM_ID = '/v2/market/item/{{id}}';
-export const SLIDESHOW = '/v2/slideshow';
-export const SEARCH = '/v2/search';
+export const FEATURED = "/v2/featured";
+export const SIDEBAR = "/v2/sidebar";
+export const MARKET_RECENT = "/v2/market/recent";
+export const MARKET_ITEM_ID = "/v2/market/item/{{id}}";
+export const SLIDESHOW = "/v2/slideshow";
+export const SEARCH = "/v2/search";
 export const GAME_DETAILS = "/v2/game";
+export const GAME_MARKET_COLLECTIONS = "v2/market/game";
+export const GAME_COLLECTION_ITEMS = "v2/market/collection";
