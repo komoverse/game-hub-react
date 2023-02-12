73b5a74f4347f97b8c2fff5649fe78b9a60f8024
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-02-13 01:05:54 +0700

    handle button login wallet

--- layouts/navbar/Navbar.tsx
+++ layouts/navbar/Navbar.tsx
@@ -1,16 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { styled } from "@mui/material/styles";
-import { AppBar, Box, IconButton, Toolbar, Button } from "@mui/material";
+import { AppBar, Box, IconButton, Toolbar, Button, Avatar, Typography, Paper, MenuList, Divider, MenuItem, ListItemIcon, ListItemText, ButtonBase } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import Image from "next/image";
 import { Iconify } from "@/components/index";
 import SearchField from "./SearchField";
 import LanguageMenu from "./LanguageMenu";
 import SearchFieldPopOver from "./SearchFieldPopover";
-
 import { APPBAR_DESKTOP, APPBAR_MOBILE } from "../constants";
 import useResponsive from "@/hooks/useResponsive";
 import { useRouter } from "next/router";
+import Login from "@/features/auth/Login";
+import actionModalAuth from '@/store/modalAuth/action'
+import { useSelector } from "react-redux";
+import { ReduxState } from "@/types/redux";
+import { shortenWalletAddress } from "@/utils/shorten";
+import { WalletsDto } from "@/types/auth";
+import { COLOR, GRADIENT } from "@/utils/globalVariable";
+import AccountPopover from "./AccountPopover";
 
 const AppbarStyled = styled(AppBar)(({ theme }) => ({
   boxShadow: "none",
@@ -34,24 +41,23 @@ function Navbar({ toggleDrawer }: NavbarProps) {
   const { t } = useTranslation();
   const isMobile = useResponsive("down", "sm");
   const router = useRouter();
+  const anchorRef = useRef(null);
 
   const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
+  const [accountPopover, setAccountPopover] = useState(false);
 
-  const toggleSearchBar = () => {
-    setIsSearchBarOpen((prev) => !prev);
-  };
+  const walletsData: WalletsDto = useSelector((state: ReduxState) => state.wallets)
+  const walletLogo = walletsData?.connector?.options.appLogoUrl
+
+  const toggleSearchBar = () => setIsSearchBarOpen((prev) => !prev);
+  const handleVisibleLogin = () => actionModalAuth.setModalAuth({ visible: true });
+  const handleOpenAccountPopover = () => setAccountPopover(true);
 
   useEffect(() => {
     if (!isMobile) {
       setIsSearchBarOpen(false);
     }
   }, [isMobile]);
-
-  const changePage = () => {
-    router.push("/komo-chess/items", "/komo-chess/items", {
-      locale: router.locale,
-    });
-  };
 
   return (
     <AppbarStyled>
@@ -109,21 +115,38 @@ function Navbar({ toggleDrawer }: NavbarProps) {
             </IconButton>
             <LanguageMenu />
             <Button
-              onClick={changePage}
+              ref={anchorRef}
+              onClick={walletsData.account !== undefined ? handleOpenAccountPopover : handleVisibleLogin}
               variant="contained"
               size="medium"
               sx={{
-                color: "#fff",
-                background:
-                  "radial-gradient(293.74% 1431.43% at -18.64% -62.88%, #99EC13 0%, #088F2E 63.54%, #054D19 100%)",
+                '&:hover': {
+                  backgroundColor: 'transparent'
+                },
+                color: COLOR.baseWhite,
+                background: walletsData.account !== undefined ? "transparent" : GRADIENT.primary,
                 borderRadius: 2,
+                border: `1px solid ${COLOR.baseGreen}`
               }}
+              startIcon={
+                walletsData.account !== undefined && <Avatar src={walletLogo} sx={{ width: 24, height: 24 }} />
+              }
             >
-              {t("navbar.login")}
+              {walletsData.account !== undefined ? (
+                <Typography variant="body2" color={COLOR.baseGreen}>({shortenWalletAddress(walletsData?.account)})</Typography>
+              ) : t("navbar.login")}
             </Button>
           </Box>
         </Box>
       </ToolbarStyled>
+
+      <Login />
+
+      <AccountPopover
+        open={accountPopover}
+        setOpen={setAccountPopover}
+        anchorRef={anchorRef}
+      />
     </AppbarStyled>
   );
 }
