65437f626d2922c4ff366a1fb38b83f88a319568
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-02-13 01:06:47 +0700

    set wagmi hooks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,8 +15,72 @@ import store from "store/store";
 import "styles/global.scss";
 import { ReactQueryDevtools } from "react-query/devtools";
 import Layout from "@/layouts/Layout";
+import { useRouter } from "next/router";
+
+import { configureChains, createClient, mainnet, WagmiConfig } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
 
 const clientSideEmotionCache = createEmotionCache();
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [publicProvider()],
+)
+
+const connectors = [
+  new InjectedConnector({
+    chains,
+    options: {
+      name: 'Komoverse Wallet',
+      shimDisconnect: true,
+      // @ts-ignore
+      appLogoUrl: 'https://avatars.githubusercontent.com/u/102501734?s=200&v=4'
+    }
+  }),
+  new MetaMaskConnector({
+    chains,
+    options: {
+      shimDisconnect: true,
+      shimChainChangedDisconnect: true,
+      // @ts-ignore
+      appLogoUrl: 'https://raw.githubusercontent.com/MetaMask/brand-resources/c3c894bb8c460a2e9f47c07f6ef32e234190a7aa/SVG/metamask-fox.svg',
+    }
+  }),
+  new CoinbaseWalletConnector({
+    // @ts-ignore
+    options: {
+      // jsonRpcUrl: 'https://eth-mainnet.alchemyapi.io/v2/yourAlchemyId',
+      appLogoUrl: 'https://avatars.githubusercontent.com/u/1885080?s=200&v=4',
+      darkMode: true,
+    },
+  }),
+  new WalletConnectConnector({
+    chains,
+    options: {
+      qrcode: true,
+      // @ts-ignore
+      appLogoUrl: 'https://raw.githubusercontent.com/WalletConnect/walletconnect-assets/c6e9d7ca2e81d4094e83849f560a024962a7987a/Icon/Blue%20(Default)/Icon.svg'
+    },
+  }),
+]
+
+const client = createClient({
+  autoConnect: true,
+  webSocketProvider,
+  provider,
+  connectors,
+  logger: {
+    warn: (message) => console.warn(message),
+  },
+})
 
 const resources = {
   en: { messages: enTranslations },
@@ -42,14 +106,12 @@ i18n.init({
   fallbackNS: [],
 });
 
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
-
 const queryClient = new QueryClient();
 
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { pathname } = useRouter()
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
@@ -61,9 +123,15 @@ export default function MyApp(props: MyAppProps) {
           <I18nextProvider i18n={i18n}>
             <QueryClientProvider client={queryClient}>
               <ReactQueryDevtools initialIsOpen={false} />
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
+              <WagmiConfig client={client}>
+                {pathname !== '/signin' ? (
+                  <Layout>
+                    <Component {...pageProps} />
+                  </Layout>
+                ) : (
+                  <Component {...pageProps} />
+                )}
+              </WagmiConfig>
             </QueryClientProvider>
           </I18nextProvider>
         </ThemeProvider>
