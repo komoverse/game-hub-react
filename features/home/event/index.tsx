bb7771efb2df3313643f016185a62bd129ba1335
Erick Hilda <erickh.andreas@gmail.com>
2023-02-14 13:37:45 +0700

    feat: fix build

--- features/home/event/index.tsx
+++ features/home/event/index.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import { Box, CardContent, Grid, Typography } from '@mui/material';
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { CardImage, SectionTitle } from '@/components/index';
-import { ButtonCard, COLOR, SectionWrapper, SectionWrapperCard } from '@/utils/globalVariable';
-import { useTranslation } from 'react-i18next';
-import { useRouter } from 'next/router';
+import React from "react";
+import { Box, CardContent, Grid, Typography } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { CardImage, SectionTitle } from "@/components/index";
+import {
+  ButtonCard,
+  COLOR,
+  SectionWrapper,
+  SectionWrapperCard,
+} from "@/utils/globalVariable";
+import { useTranslation } from "react-i18next";
+import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
-import { shortenTitleGame } from '@/utils/shorten';
-import { getDiff, isBefore, isBetween } from '@/helper/date';
-import { EventTypes, MintScheduleDto, ScheduleEvents, TournamentScheduleDto } from '@/types/home';
-import { ReduxState } from '@/types/redux';
+import { shortenTitleGame } from "@/utils/shorten";
+import { getDiff, isBefore, isBetween } from "@/helper/date";
+import {
+  EventTypes,
+  MintScheduleDto,
+  ScheduleEvents,
+  TournamentScheduleDto,
+} from "@/types/home";
+import { ReduxState } from "@/types/redux";
 import { Navigation } from "swiper";
 
 const Events = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   const router = useRouter();
-  const data: any = useSelector((state: ReduxState) => state.sidebar?.value)
+  const data: any = useSelector((state: ReduxState) => state.sidebar?.value);
   const currDate = new Date().toISOString();
 
   const transformMintSchedule = (item: MintScheduleDto) => ({
@@ -34,32 +44,41 @@ const Events = () => {
     end_time: item.end_time,
   });
 
-  const transformedData: (MintScheduleDto)[] = [
-    ...(Array.isArray(data?.mint_schedule) ? data.mint_schedule.map(transformMintSchedule) : []),
-    ...(Array.isArray(data?.tournament) ? data.tournament.map(transformTournament) : []),
+  const transformedData: MintScheduleDto[] = [
+    ...(Array.isArray(data?.mint_schedule)
+      ? data.mint_schedule.map(transformMintSchedule)
+      : []),
+    ...(Array.isArray(data?.tournament)
+      ? data.tournament.map(transformTournament)
+      : []),
   ];
 
   const filteredDataByTime = transformedData.sort((a, b) => {
-    const dateA = a.type === EventTypes.MINTS ? a.mint_start_date : a.start_time;
-    const dateB = b.type === EventTypes.MINTS ? b.mint_start_date : b.start_time;
+    const dateA =
+      a.type === EventTypes.MINTS ? a.mint_start_date : a.start_time;
+    const dateB =
+      b.type === EventTypes.MINTS ? b.mint_start_date : b.start_time;
     return dateA < dateB ? -1 : dateA > dateB ? 1 : 0;
-  })
+  });
 
-  const handleRedirect = (data: ScheduleEvents, event?: React.MouseEventHandler<HTMLDivElement>) => {
-    const { type, game_id } = data
+  const handleRedirect = (
+    data: ScheduleEvents,
+    event?: React.MouseEventHandler<HTMLDivElement>
+  ) => {
+    const { type, game_id } = data;
     if (type === EventTypes.MINTS) {
-      router.push(`/${game_id}/mint`)
+      router.push(`/${game_id}/mint`);
     }
     if (type === EventTypes.TOURNAMENTS) {
-      router.push(`/${game_id}/tournament`)
+      router.push(`/${game_id}/tournament`);
     }
-  }
+  };
 
   return (
     <SectionWrapper>
       <SectionWrapperCard>
-        <SectionTitle title={t('home.event')} />
-        <Box sx={{ position: 'relative' }}>
+        <SectionTitle title={t("home.event")} />
+        <Box sx={{ position: "relative" }}>
           <Swiper
             slidesPerView="auto"
             loopedSlides={4}
@@ -67,7 +86,7 @@ const Events = () => {
             preloadImages={false}
             lazy={true}
             pagination={{
-              el: '.swiper-pagination',
+              el: ".swiper-pagination",
               clickable: true,
             }}
             navigation={true}
@@ -76,15 +95,15 @@ const Events = () => {
             className="events-swipper"
           >
             {filteredDataByTime?.map((event, idx: number) => {
-              const startTime = event.mint_start_date || event.start_time
-              const endTime = event.mint_end_date || event.end_time
-              const eventName = event.phase_name || event.tournament_name
+              const startTime = event.mint_start_date || event.start_time;
+              const endTime = event.mint_end_date || event.end_time;
+              const eventName = event.phase_name || event.tournament_name;
 
               const time = isBefore(currDate, startTime)
-                ? `${Math.abs(getDiff(currDate, startTime))} ${t('time.days')}`
+                ? `${Math.abs(getDiff(currDate, startTime))} ${t("time.days")}`
                 : isBetween(currDate, startTime, endTime)
-                  ? t('button.live')
-                  : "-"
+                ? t("button.live")
+                : "-";
 
               return (
                 <SwiperSlide key={idx}>
@@ -102,15 +121,25 @@ const Events = () => {
                             textAlign: "center",
                           }}
                         >
-                          <Typography variant='h6' sx={{ fontWeight: 700 }}>
+                          <Typography variant="h6" sx={{ fontWeight: 700 }}>
                             {shortenTitleGame(event.game_name)}
                           </Typography>
-                          <Typography variant='subtitle2' sx={{ fontWeight: 400, color: COLOR.baseSemiGray }}>
+                          <Typography
+                            variant="subtitle2"
+                            sx={{ fontWeight: 400, color: COLOR.baseSemiGray }}
+                          >
                             {shortenTitleGame(eventName)}
                           </Typography>
                         </CardContent>
                         <ButtonCard>
-                          <Typography variant='subtitle2' sx={{ fontWeight: 700, marginLeft: 1, color: COLOR.baseGreen }}>
+                          <Typography
+                            variant="subtitle2"
+                            sx={{
+                              fontWeight: 700,
+                              marginLeft: 1,
+                              color: COLOR.baseGreen,
+                            }}
+                          >
                             {time}
                           </Typography>
                         </ButtonCard>
@@ -118,13 +147,14 @@ const Events = () => {
                     </Grid>
                   </Grid>
                 </SwiperSlide>
-              )
+              );
             })}
           </Swiper>
         </Box>
       </SectionWrapperCard>
     </SectionWrapper>
-  )
-}
+  );
+};
+const memoizedEvents = React.memo(Events);
 
-export default React.memo(Events)
+export default memoizedEvents;
