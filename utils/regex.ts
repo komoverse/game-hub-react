3ee87fdf6cf33e9309b7d7b44490801d92346ad5
Erick Hilda <erickh.andreas@gmail.com>
2023-02-21 23:06:04 +0700

    fix: fix regex for komo_username

--- utils/regex.ts
+++ utils/regex.ts
@@ -2,5 +2,5 @@ export const regexUrlValidation =
   /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
 export const regexImageFileValidation = /\.(gif|jpg|jpeg|tiff|png|webp)$/i;
 export const komoAccountUsernameValidation =
-  /^(?=.{4,30}$)(?![.])(?!.*[.]{2})[a-zA-Z0-9.]+(?<![.])$/i;
+  /^(?=.{6,30}$)(?![.])(?!.*[.]{2})[a-zA-Z0-9.]+(?<![.])$/i;
 export const emailValidation = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i;
