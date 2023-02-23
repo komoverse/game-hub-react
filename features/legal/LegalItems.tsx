7f96b6c4cf07011a71261251e10dcd516533df6b
Erick Hilda <erickh.andreas@gmail.com>
2023-02-23 12:37:45 +0700

    feat: finish tou page and create legal content component

---
+++ features/legal/LegalItems.tsx
@@ -0,0 +1,42 @@
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { ReactNode } from 'react';
+
+import { StyledBodyText, StyledSubheading } from './styles';
+
+export const LegalSectionContainer = ({
+  children,
+  index,
+  title,
+}: {
+  children: ReactNode;
+  index: string;
+  title: string;
+}) => (
+  <Grid container spacing={2} mt={4}>
+    <Grid item md={4} sx={{ textAlign: 'left' }}>
+      <Box sx={{ display: 'flex', gap: '8px' }}>
+        <StyledSubheading>{index}.</StyledSubheading>
+        <StyledSubheading>{title}</StyledSubheading>
+      </Box>
+    </Grid>
+    <Grid item md={8} sx={{ textAlign: 'justify' }}>
+      {children}
+    </Grid>
+  </Grid>
+);
+
+export const LegalSectionContent = ({
+  children,
+  subIndex,
+}: {
+  children: ReactNode;
+  subIndex: string;
+}) => {
+  return (
+    <Box sx={{ display: 'flex', gap: '20px' }}>
+      <StyledBodyText>{subIndex}</StyledBodyText>
+      <Box>{children}</Box>
+    </Box>
+  );
+};
