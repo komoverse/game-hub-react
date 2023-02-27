66e5f41524af45754b451992e4770dad6df529fd
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-02-27 23:33:44 +0700

    grouping env

--- helper/env.ts
+++ helper/env.ts
@@ -2,3 +2,6 @@ export const recaptchaSiteKey =
   process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '';
 export const recaptchaSecretKey =
   process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY || '';
+export const BASE_URL_API = process.env.NEXT_PUBLIC_BASE_URL_API || '';
+export const BASE_URL_API_KEY = process.env.NEXT_PUBLIC_API_KEY || '';
+export const PASSPHRASE = process.env.NEXT_PUBLIC_PASSPHRASE || '';
