c010121026659bc2c4fc505bc6aa5cdf3fa9a963
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2023-02-28 22:09:24 +0700

    add data types for review

--- types/game/index.ts
+++ types/game/index.ts
@@ -47,15 +47,60 @@ export type SummaryDto = {
   total_reviews: number;
 };
 
+export type OverallSumaryDto = {
+  overall_rating: number;
+  total_reviews: number;
+};
+
+export type RatingSummaryDto = {
+  rating: number;
+  total_rating: number;
+};
+
+export type LikeDislikeByMeDto = {
+  id: number;
+  game_review_id: number;
+  komo_username: string;
+  like: number;
+  dislike: number;
+  created_at: string | null;
+  updated_at: string | null;
+  game_reviews: {
+    id: number;
+    komo_username: string;
+    game_id: number;
+    rating: number;
+    comment: string;
+    reviewer_picture_url: string;
+    created_at: string | string;
+  };
+};
+
+export type ReviewByMeDto = {
+  id: number;
+  komo_username: string;
+  game_id: string;
+  rating: number;
+  comment: string;
+  reviewer_picture_url: string;
+  created_at: string;
+  like_comment_count: number;
+  dislike_comment_count: number;
+};
+
 export interface ListReviewsDto {
+  disliked_by_me: LikeDislikeByMeDto[];
+  liked_by_me: LikeDislikeByMeDto[];
   monthly_stats: ReviewsMonthlyStatsDto;
+  overall_summary: OverallSumaryDto;
+  rating_summary: RatingSummaryDto[];
   reviews: {
     data: ReviewsDto[];
     total: number;
     to: number;
     current_page: number;
   };
-  reviewed_by_me: string | null;
+  reviewed_by_me: ReviewByMeDto;
   summary: SummaryDto;
   length: number;
 }
