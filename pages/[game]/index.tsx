764f094301222e1882f8320fe710eb60416f4dc6
erickproject <122963694+erickproject@users.noreply.github.com>
2023-03-14 20:53:59 +0700

    Fix/remove dynamic import for page (#73)
    * fix: not using dynamic import for page level component
    
    * fix: dynamic import overview page
    
    * fix: add fallback for dynamic import overview page
    
    ---------
    
    Co-authored-by: Erick Hilda <erickh.andreas@gmail.com>

--- pages/[game]/index.tsx
+++ pages/[game]/index.tsx
@@ -1,6 +1,8 @@
 import dynamic from 'next/dynamic';
 
-const Overview = dynamic(() => import('@/features/game/Overview'));
+const Overview = dynamic(() => import('@/features/game/Overview'), {
+  loading: () => <p>Loading...</p>,
+});
 
 export default function OverviewPages() {
   return <Overview />;
